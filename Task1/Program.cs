09f9afab217c8a717f39e406c7fb1a184e5b3573
Anton <ianton558@gmail.com>
2021-01-12 12:51:52 +0200

    Exam 3d tasl

---
+++ Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static string input;
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            input = Console.ReadLine();
+            Task.Run(() => PrintAsync());
+
+            while (true)
+            {
+                if (Console.ReadLine() == "")
+                {
+                    lock (locker)
+                    {
+                        input = Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        private static async Task PrintAsync()
+        {
+            while (true)
+            {
+                lock (locker)
+                {
+                    Console.WriteLine(input);
+                }
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
